7def65ca922f3ec73753f56cdb48eac0cffb439c
YasserRohaim <yasser.rohaim9@gmail.com>
2024-11-26 09:59:13 +0200

    moved the the userSchema and implemented a validation func in the middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,19 @@
 const User = require('../models/User');
-const { object, string, number, date } =require('yup');
+const { object, string, number } =require('yup');
 
 var userSchema = object({
     name: string().required(),
     password:string().min(8,"minimum length is 8").required(),
     email: string().email(),
     budget: number.positive(),
-    preferredCurrency : number().min(0).max(100)
+    preferredCurrency :  mixed()
+    .oneOf(['USD', 'EUR', 'JPY', 'AED','EGP','SAR'], 'Invalid currency') 
+    .required('Preferred currency is required'),
   });
 exports.createUser = async (req, res) => {
   try {
     const { name, email, password, budget, preferredCurrency } = req.body;
+    userSchema.validate()
     
     const user = new User({ name, email, passwordHash, budget, preferredCurrency });
     await user.save();
